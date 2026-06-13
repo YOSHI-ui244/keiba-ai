2bdd63e17d8d7a25476c23885aa95da12ef9655d
yoshikawa <waposaomega@icloud.com>
2026-06-13 09:31:37 +0900

    auto: 当日版へ昇格・前日アーカイブ・成績更新

--- today.js
+++ today.js
@@ -1,1 +1,1 @@
-window.TODAY_VENUES = {"date": "20260612", "venues": {"oi": "大井", "kasamatsu": "笠松", "sonoda": "園田"}};
+window.TODAY_VENUES = {"date": "20260613", "venues": {"kochi": "高知", "saga": "佐賀"}};
